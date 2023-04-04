cade16c9d90edbbad73061eee12fb91967b8f9ea
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2023-04-04 15:36:07 +0900

    flipper meta context

---
+++ contracts/flipper/lib.rs
@@ -0,0 +1,178 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![feature(min_specialization)]
+
+#[ink::contract]
+mod flipper {
+    use ink::prelude::vec::Vec;
+    use openbrush::{
+        traits::{
+            Storage,
+        },
+        contracts::access_control::*,
+    };
+    use meta_tx_context::*;
+
+    /// Defines the storage of your contract.
+    /// Add new fields to the below struct in order
+    /// to add new static storage fields to your contract.
+    #[ink(storage)]
+    #[derive(Default, Storage)]
+    pub struct Flipper {
+        #[storage_field]
+        access_control: access_control::Data,
+        #[storage_field]
+        meta_tx_context: meta_tx_context::Data,
+
+        /// Stores a single `bool` value on the storage.
+        value: bool,
+    }
+
+    impl AccessControl for Flipper {}
+    impl MetaTxContext for Flipper {}
+
+    /// Errors that can occur upon calling this contract.
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(::scale_info::TypeInfo))]
+    pub enum Error {
+        MetaTxContextError(meta_tx_context::Error),
+    }
+
+    impl From<meta_tx_context::Error> for Error {
+        fn from(err: meta_tx_context::Error) -> Self {
+            Error::MetaTxContextError(err)
+        }
+    }
+
+    impl Flipper {
+        /// Constructor that initializes the `bool` value to the given `init_value`.
+        #[ink(constructor)]
+        pub fn new(trusted_forwarder: AccountId, init_value: bool) -> Self {
+            let mut _instance = Self::default();
+            _instance._init_with_admin(_instance.env().caller());
+            _instance.set_trusted_forwarder(trusted_forwarder).expect("Should have MANAGER role");
+            _instance.value = init_value;
+            _instance
+        }
+
+        /// A message that can be called on instantiated contracts.
+        /// This one flips the value of the stored `bool` from `true`
+        /// to `false` and vice versa.
+        #[ink(message)]
+        pub fn flip(&mut self) {
+            self.value = !self.value;
+        }
+
+        // Same flip functionality with meta context.
+        #[ink(message)]
+        pub fn flip_meta_context(&mut self, data: Vec<u8>) -> Result<(), Error> {
+            let caller = self._caller(data)?;
+            ink::env::debug_println!("Flip called by {:?}", caller);
+            self.value = !self.value;
+            Ok(())
+        }
+
+        /// Simply returns the current value of our `bool`.
+        #[ink(message)]
+        pub fn get(&self) -> bool {
+            self.value
+        }
+    }
+
+    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
+    /// module and test functions are marked with a `#[test]` attribute.
+    /// The below code is technically just normal Rust code.
+    #[cfg(test)]
+    mod tests {
+        /// Imports all the definitions from the outer scope so we can use them here.
+        use super::*;
+
+        /// We test if the default constructor does its job.
+        #[ink::test]
+        fn default_works() {
+            let flipper = Flipper::default();
+            assert_eq!(flipper.get(), false);
+        }
+
+        /// We test a simple use case of our contract.
+        #[ink::test]
+        fn it_works() {
+            let mut flipper = Flipper::new(false);
+            assert_eq!(flipper.get(), false);
+            flipper.flip();
+            assert_eq!(flipper.get(), true);
+        }
+    }
+
+
+    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
+    ///
+    /// When running these you need to make sure that you:
+    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
+    /// - Are running a Substrate node which contains `pallet-contracts` in the background
+    #[cfg(all(test, feature = "e2e-tests"))]
+    mod e2e_tests {
+        /// Imports all the definitions from the outer scope so we can use them here.
+        use super::*;
+
+        /// A helper function used for calling contract messages.
+        use ink_e2e::build_message;
+
+        /// The End-to-End test `Result` type.
+        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+        /// We test that we can upload and instantiate the contract using its default constructor.
+        #[ink_e2e::test]
+        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            // Given
+            let constructor = FlipperRef::default();
+
+            // When
+            let contract_account_id = client
+                .instantiate("flipper", &ink_e2e::alice(), constructor, 0, None)
+                .await
+                .expect("instantiate failed")
+                .account_id;
+
+            // Then
+            let get = build_message::<FlipperRef>(contract_account_id.clone())
+                .call(|flipper| flipper.get());
+            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
+            assert!(matches!(get_result.return_value(), false));
+
+            Ok(())
+        }
+
+        /// We test that we can read and write a value from the on-chain contract contract.
+        #[ink_e2e::test]
+        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            // Given
+            let constructor = FlipperRef::new(false);
+            let contract_account_id = client
+                .instantiate("flipper", &ink_e2e::bob(), constructor, 0, None)
+                .await
+                .expect("instantiate failed")
+                .account_id;
+
+            let get = build_message::<FlipperRef>(contract_account_id.clone())
+                .call(|flipper| flipper.get());
+            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
+            assert!(matches!(get_result.return_value(), false));
+
+            // When
+            let flip = build_message::<FlipperRef>(contract_account_id.clone())
+                .call(|flipper| flipper.flip());
+            let _flip_result = client
+                .call(&ink_e2e::bob(), flip, 0, None)
+                .await
+                .expect("flip failed");
+
+            // Then
+            let get = build_message::<FlipperRef>(contract_account_id.clone())
+                .call(|flipper| flipper.get());
+            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
+            assert!(matches!(get_result.return_value(), true));
+
+            Ok(())
+        }
+    }
+}
